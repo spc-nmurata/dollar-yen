a4d27d7e9fdbad5bfe89f01a0c29b90f7a5fb1d8
spc-nmurata <nmurata@sciencepark.co.jp>
2024-06-16 13:32:32 +0900

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dollar-yen",  # パッケージ名
-    version="0.0.1",
+    version="0.0.2",
     author="naoyuki murata",
     author_email="nmurata@sciencepark.co.jp",
     description="Convert dollars to yen.",
